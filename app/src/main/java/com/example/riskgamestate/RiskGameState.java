ae9d88501f845cce0e91ce85a03fe154eef96e19
phin2 <nguyephi24@up.edu>
2021-09-27 12:20:11 -0700

    Started attack method

--- app/src/main/java/com/example/riskgamestate/RiskGameState.java
+++ app/src/main/java/com/example/riskgamestate/RiskGameState.java
@@ -6,15 +6,19 @@ public class RiskGameState {
 
     public static final int MAX_PLAYERS = 4;
     public static final int MIN_PLAYERS = 2;
-
     private ArrayList<Territory> territories;
     private ArrayList<Card> cards;
 
     public RiskGameState() {
-
         // initialize territories
-
         // add adjacents to each territory
+    }
+
+    public RiskGameState(RiskGameState other) {
+
+    }
+
+    public void attack(int playerID,Territory attacking,Territory attacked) {
 
     }
 
